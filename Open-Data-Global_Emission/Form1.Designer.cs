7bfa389eac46a31ba64c922c0e65033a7be466d8
Riccardi.20248 <Riccardi.20248@LABI1-6PC07>
2024-10-22 09:33:27 +0200

    correzione parte grafica del form

--- Open-Data-Global_Emission/Form1.Designer.cs
+++ Open-Data-Global_Emission/Form1.Designer.cs
@@ -53,45 +53,48 @@
             this.listView1.HideSelection = false;
             this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
             listViewItem1});
-            this.listView1.Location = new System.Drawing.Point(28, 32);
+            this.listView1.Location = new System.Drawing.Point(12, 181);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(503, 522);
+            this.listView1.Size = new System.Drawing.Size(876, 369);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // btnVisualizzaCsv
             // 
-            this.btnVisualizzaCsv.Location = new System.Drawing.Point(537, 32);
+            this.btnVisualizzaCsv.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnVisualizzaCsv.Location = new System.Drawing.Point(10, 12);
             this.btnVisualizzaCsv.Name = "btnVisualizzaCsv";
-            this.btnVisualizzaCsv.Size = new System.Drawing.Size(70, 67);
+            this.btnVisualizzaCsv.Size = new System.Drawing.Size(109, 67);
             this.btnVisualizzaCsv.TabIndex = 1;
             this.btnVisualizzaCsv.Text = "Apri File";
-            this.btnVisualizzaCsv.UseVisualStyleBackColor = true;
+            this.btnVisualizzaCsv.UseVisualStyleBackColor = false;
             this.btnVisualizzaCsv.Click += new System.EventHandler(this.btnVisualizzaCsv_Click);
             // 
             // BtnFilterRegion
             // 
-            this.BtnFilterRegion.Location = new System.Drawing.Point(645, 126);
+            this.BtnFilterRegion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnFilterRegion.Location = new System.Drawing.Point(119, 112);
             this.BtnFilterRegion.Name = "BtnFilterRegion";
-            this.BtnFilterRegion.Size = new System.Drawing.Size(113, 20);
+            this.BtnFilterRegion.Size = new System.Drawing.Size(113, 63);
             this.BtnFilterRegion.TabIndex = 2;
             this.BtnFilterRegion.Text = "Filtra Per Regione";
-            this.BtnFilterRegion.UseVisualStyleBackColor = true;
+            this.BtnFilterRegion.UseVisualStyleBackColor = false;
             this.BtnFilterRegion.Click += new System.EventHandler(this.BtnFilterRegion_Click);
             // 
             // txtRegionFilter
             // 
-            this.txtRegionFilter.Location = new System.Drawing.Point(539, 126);
+            this.txtRegionFilter.Location = new System.Drawing.Point(13, 112);
+            this.txtRegionFilter.Multiline = true;
             this.txtRegionFilter.Name = "txtRegionFilter";
-            this.txtRegionFilter.Size = new System.Drawing.Size(100, 20);
+            this.txtRegionFilter.Size = new System.Drawing.Size(100, 63);
             this.txtRegionFilter.TabIndex = 3;
             this.txtRegionFilter.TextChanged += new System.EventHandler(this.txtRegionFilter_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(539, 110);
+            this.label1.Location = new System.Drawing.Point(13, 96);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(219, 13);
             this.label1.TabIndex = 4;
@@ -101,7 +104,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(537, 162);
+            this.label2.Location = new System.Drawing.Point(238, 96);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(205, 13);
             this.label2.TabIndex = 7;
@@ -109,63 +112,69 @@
             // 
             // txtCountryFilter
             // 
-            this.txtCountryFilter.Location = new System.Drawing.Point(537, 178);
+            this.txtCountryFilter.Location = new System.Drawing.Point(238, 112);
+            this.txtCountryFilter.Multiline = true;
             this.txtCountryFilter.Name = "txtCountryFilter";
-            this.txtCountryFilter.Size = new System.Drawing.Size(100, 20);
+            this.txtCountryFilter.Size = new System.Drawing.Size(100, 63);
             this.txtCountryFilter.TabIndex = 6;
             this.txtCountryFilter.TextChanged += new System.EventHandler(this.txtCountryFilter_TextChanged);
             // 
             // BtnFilterCountry
             // 
-            this.BtnFilterCountry.Location = new System.Drawing.Point(643, 178);
+            this.BtnFilterCountry.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnFilterCountry.Location = new System.Drawing.Point(348, 112);
             this.BtnFilterCountry.Name = "BtnFilterCountry";
-            this.BtnFilterCountry.Size = new System.Drawing.Size(113, 20);
+            this.BtnFilterCountry.Size = new System.Drawing.Size(104, 63);
             this.BtnFilterCountry.TabIndex = 5;
             this.BtnFilterCountry.Text = "Filtra Per Paese";
-            this.BtnFilterCountry.UseVisualStyleBackColor = true;
+            this.BtnFilterCountry.UseVisualStyleBackColor = false;
             this.BtnFilterCountry.Click += new System.EventHandler(this.BtnFilterCountry_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(613, 32);
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(125, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 67);
+            this.button1.Size = new System.Drawing.Size(107, 67);
             this.button1.TabIndex = 8;
             this.button1.Text = "Ordinamento Emissioni Decrescente";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(719, 32);
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(238, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 67);
+            this.button2.Size = new System.Drawing.Size(104, 67);
             this.button2.TabIndex = 9;
             this.button2.Text = "Ordinamento Emissioni Crescente";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // BtnFilterYear
             // 
-            this.BtnFilterYear.Location = new System.Drawing.Point(643, 226);
+            this.BtnFilterYear.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnFilterYear.Location = new System.Drawing.Point(567, 112);
             this.BtnFilterYear.Name = "BtnFilterYear";
-            this.BtnFilterYear.Size = new System.Drawing.Size(113, 20);
+            this.BtnFilterYear.Size = new System.Drawing.Size(104, 63);
             this.BtnFilterYear.TabIndex = 10;
             this.BtnFilterYear.Text = "Filtra per Anno";
-            this.BtnFilterYear.UseVisualStyleBackColor = true;
+            this.BtnFilterYear.UseVisualStyleBackColor = false;
             this.BtnFilterYear.Click += new System.EventHandler(this.BtnFilterYear_Click);
             // 
             // txtYearFilter
             // 
-            this.txtYearFilter.Location = new System.Drawing.Point(537, 226);
+            this.txtYearFilter.Location = new System.Drawing.Point(461, 112);
+            this.txtYearFilter.Multiline = true;
             this.txtYearFilter.Name = "txtYearFilter";
-            this.txtYearFilter.Size = new System.Drawing.Size(100, 20);
+            this.txtYearFilter.Size = new System.Drawing.Size(100, 63);
             this.txtYearFilter.TabIndex = 11;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(534, 210);
+            this.label3.Location = new System.Drawing.Point(458, 96);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(204, 13);
             this.label3.TabIndex = 12;
@@ -173,12 +182,13 @@
             // 
             // BtnCalcolaStatistiche
             // 
-            this.BtnCalcolaStatistiche.Location = new System.Drawing.Point(537, 264);
+            this.BtnCalcolaStatistiche.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnCalcolaStatistiche.Location = new System.Drawing.Point(348, 12);
             this.BtnCalcolaStatistiche.Name = "BtnCalcolaStatistiche";
-            this.BtnCalcolaStatistiche.Size = new System.Drawing.Size(100, 67);
+            this.BtnCalcolaStatistiche.Size = new System.Drawing.Size(104, 67);
             this.BtnCalcolaStatistiche.TabIndex = 13;
             this.BtnCalcolaStatistiche.Text = "Media Emissioni";
-            this.BtnCalcolaStatistiche.UseVisualStyleBackColor = true;
+            this.BtnCalcolaStatistiche.UseVisualStyleBackColor = false;
             this.BtnCalcolaStatistiche.Click += new System.EventHandler(this.BtnCalcolaStatistiche_Click);
             // 
             // Form1
